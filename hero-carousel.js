c9e8724573798b2ce58705efc54933233cf0728a
yihyoni <gugithub@gmail.com>
2026-05-06 17:06:42 +0900

    슬라이드 2초 지나면 다음사진

--- hero-carousel.js
+++ hero-carousel.js
@@ -3,10 +3,12 @@
 const heroTrack = document.querySelector(".hero-carousel-track");
 const dots = document.querySelectorAll(".dot");
 // const totalSlides = 3; // 총 슬라이드 개수
+let currentIndex = 0; // 지금 몇 번째 슬라이드인지 인덱스로 구별
 
 // 클릭 이벤트 연결
 dots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
+    currentIndex = index; // 클릭된 인덱스를 현재 슬라이드에 저장
     heroTrack.style.transform = `translateX(${-index * 100}vw)`;
 
     // 클릭한 dot 에만 active 부여
@@ -14,3 +16,11 @@ dots.forEach((dot, index) => {
     dot.classList.add("active");
   });
 });
+
+// 2초마다 자동 슬라이드
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % dots.length; // 나머지값 구해서 인덱스 순환
+  heroTrack.style.transform = `translateX(${-currentIndex * 100}vw)`;
+  dots.forEach((d) => d.classList.remove("active")); // 모든 dot 에서 active 제거
+  dots[currentIndex].classList.add("active");
+}, 2000);
